e51c1e5eabfdc38a3690c6a568d9a175f67ef83d
Siwatchaya555 <52107730+Siwatchaya555@users.noreply.github.com>
2019-06-30 21:59:04 +0700

    Update line.php

--- line.php
+++ line.php
@@ -2,7 +2,7 @@
   
 
 function send_LINE($msg){
- $access_token = 'qLip9omRdSnsaKFlsWmCCx9pdvAcRd1CGb6XfH/K3aKVgmHS4Eh/a35I8S1q8XVCZQVJUVIPa2B/c1ZJHfEyA8vUgqlUeIfqTkw607IKQ7yCasUHW34wj+CGzB6bOafYNDSGkh87GIr+Tns7fqFqVAdB04t89/1O/w1cDnyilFU='; 
+ $access_token = 'G9hXF8vurFGkurRN0vZ8aWYJdj7xiueB2Zat2pxBvfwk6lUt4JWios1R1WQH7VPv5fzIDiqPVZLdzwQacImuDYpstQyHpqTR0tTTDebov8cv12iCyFxcMoZGikmTATNOrQmm9Qs2yVunCNMmPKG1WwdB04t89/1O/w1cDnyilFU='; 
 
   $messages = [
         'type' => 'text',
@@ -14,7 +14,7 @@ function send_LINE($msg){
       $url = 'https://api.line.me/v2/bot/message/push';
       $data = [
 
-        'to' => 'Ue77a191627f6ac91899e75d92264310c',
+        'Userid' => 'U69d3ba8b7205cf4a9b9ca0944e1bf8c5',
         'messages' => [$messages],
       ];
       $post = json_encode($data);
